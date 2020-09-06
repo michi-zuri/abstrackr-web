98eb8e480bf3cfe5644e38ee074b95a30dd7c418
Michael Killian <killian@posteo.de>
2020-09-06 13:40:41 +0200

    more pruning

--- abstrackr/exporter_globals.py
+++
@@ -1,8 +0,0 @@
-from pylons import config
-
-## GLOBALS
-ROOT_PATH = config['pylons.paths']['root']
-STATIC_FILES_PATH = config['pylons.paths']['static_files']
-
-# this is the path where uploaded databases will be written to
-PERMANENT_STORE = STATIC_FILES_PATH + "/uploads"
